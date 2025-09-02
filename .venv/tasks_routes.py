183bd90ba2ad4bec1caae41f7542e32eb34bb976
Vinicius-Muxila <viniciuss.nuness@gmail.com>
2025-09-01 21:45:04 -0300

    atualização rota tarefas

--- .venv/tasks_routes.py
+++ .venv/tasks_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
-tasks_router = APIRouter(prefix="/tasks", tags=["tasks"])
+tasks_router = APIRouter(prefix="/tarefas", tags=["tarefas"])
 
 @tasks_router.get("/tarefas")
-async def get_tasks():
+async def mostrar_tarefas():
     return {"tasks": ["Tarefa 1", "Tarefa 2", "Tarefa 3"]}
