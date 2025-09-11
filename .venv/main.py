09eb6628aeab968fa3616b67e7dfd69a240f33c8
Vinicius-Muxila <viniciuss.nuness@gmail.com>
2025-09-11 20:31:37 -0300

    Iniciando processo de criptografia

--- .venv/main.py
+++ .venv/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from passlib.context import CryptContext 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 app = FastAPI()
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 from auth_routes import auth_router
 from tasks_routes import tasks_router
