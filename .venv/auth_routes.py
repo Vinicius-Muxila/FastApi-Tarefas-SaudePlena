8a6990ca1329bd761b4c27ae7d25f19bcb73c9d8
Vinicius-Muxila <viniciuss.nuness@gmail.com>
2025-10-08 21:37:26 -0300

    alteração simples do código

--- .venv/auth_routes.py
+++ .venv/auth_routes.py
@@ -29,19 +29,5 @@ async def criar_conta(usuario_schema: UsuarioSchema, session: Session = Depends(
     
 
 
-# @auth_router.post("/criar_conta")
-# async def criar_conta(usuario_schema: UsuarioSchema, session = Depends(pegar_sessao)):
-#     usuario_existente = session.query(Usuario).filter(Usuario.email == usuario_schema.email).first()
-#     if Usuario:
-#         # ja existe usuario com esse email
-#         raise HTTPException(status_code=400, detail="já existe um usuário com esse email")
-#     else:
-#         senha_criptografada = bcrypt_context.hash(usuario_schema.senha)
-#         novo_usuario = Usuario(usuario_schema.nome, usuario_schema.email, senha_criptografada, usuario_schema.ativo, usuario_schema.admin)
-#         session.add(novo_usuario)
-#         session.commit()
-#         return {"mensagem": f"usuario cadastrado com sucesso!{usuario_schema.email}"}
-
-
 
 
