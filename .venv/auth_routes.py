e939bca8c6ecf6571b57f250cb8d61a59eb36681
Vinicius-Muxila <viniciuss.nuness@gmail.com>
2025-10-21 22:15:03 -0300

    alteração auth

--- .venv/auth_routes.py
+++ .venv/auth_routes.py
@@ -11,6 +11,15 @@ def criar_token(id_usuario):
     token = f'dskdmfmdsf{str(id_usuario)}fmdsfmksdmf'
     return token
 
+def autenticar_usuario(email, senha, session):
+    usuario = session.query(Usuario).filter(Usuario.email == email.email).first()
+    if not usuario:
+        return False
+    elif not bcrypt_context.verify(senha, usuario.senha):
+        return False
+
+    return usuario
+
 @auth_router.get("/")
 async def home(): 
     """
@@ -33,7 +42,7 @@ async def criar_conta(usuario_schema: UsuarioSchema, session: Session = Depends(
     
 @auth_router.post("/login")
 async def login(login_schema: LoginSchema, session: Session = Depends(pegar_sessao)):
-    usuario = session.query(Usuario).filter(Usuario.email == login_schema.email).first()
+    usuario = autenticar_usuario(login_schema.email, login_schema.senha, session)
     if not usuario:
         raise HTTPException(status_code=400, detail="Usuário não encontrado")
     else:
